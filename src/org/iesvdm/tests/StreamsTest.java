e1bdcc62e2c36a1898c94d31243a33e58f757c64
usuario <usuario@DESKTOP-UL3D53O>
2022-12-30 11:46:40 +0100

    init

--- src/org/iesvdm/tests/StreamsTest.java
+++ src/org/iesvdm/tests/StreamsTest.java
@@ -2,12 +2,20 @@ package org.iesvdm.tests;
 
 import static org.junit.jupiter.api.Assertions.fail;
 import static java.util.stream.Collectors.*;
+import static java.util.Comparator.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import org.iesvdm.streams.Cliente;
 import org.iesvdm.streams.ClienteHome;
@@ -106,10 +114,7 @@ class StreamsTest {
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 			Date ultimoDia2016 = sdf.parse("2016-12-31");
 			
-		
 			List<Pedido> list = pedHome.findAll();
-			
-
 				
 			//TODO STREAMS	
 						
@@ -137,11 +142,9 @@ class StreamsTest {
 	
 			List<Cliente> list = cliHome.findAll();
 			
+			//TODO STREAMS		
 		
 			cliHome.commitTransaction();
-			
-			
-		
 			
 		}
 		catch (RuntimeException e) {
@@ -163,10 +166,9 @@ class StreamsTest {
 		
 			List<Comercial> list = comHome.findAll();		
 			
+			//TODO STREAMS		
+				
 			comHome.commitTransaction();
-			
-			Float maxComision = list.stream()
-									.collect(maxBy(Comparator.comparingDouble(Comercial::getComisión)));
 			
 		}
 		catch (RuntimeException e) {
@@ -190,16 +192,9 @@ class StreamsTest {
 	
 			List<Cliente> list = cliHome.findAll();
 			
-		
+			//TODO STREAMS
+			
 			cliHome.commitTransaction();
-			
-			var listado = list.stream()
-								.filter(c -> c.getApellido2() != null)
-								.map(c -> c.getId() + c.getNombre()+ c.getApellido1() )
-								.sorted(Cliente::getApellido1)
-								//.collect(toList())
-								;
-			
 			
 		}
 		catch (RuntimeException e) {
@@ -222,6 +217,8 @@ class StreamsTest {
 		
 			List<Comercial> list = comHome.findAll();		
 			
+			//TODO STREAMS
+			
 			comHome.commitTransaction();
 		}
 		catch (RuntimeException e) {
@@ -243,7 +240,6 @@ class StreamsTest {
 			pedHome.beginTransaction();
 		
 			List<Pedido> list = pedHome.findAll();
-			
 						
 			//TODO STREAMS
 		
@@ -270,16 +266,11 @@ class StreamsTest {
 			comHome.beginTransaction();
 			
 			List<Comercial> list = comHome.findAll();		
+			
+			//TODO STREAMS
+			
 			comHome.commitTransaction();
 			
-			//List <Pedido> pedidos = pedHome.findAll();
-			//id_comercial ==1
-			//var mediaDanielSaez = pedidos.stream()
-											//.filter(p -> p.getComercial() == 1);
-			
-			Long mediaDanielSaez = list.stream()
-										.filter(c->c.getNombre().equals("Daniel")&c.getApellido1().equals("Sáez"))
-										.collect(averagingLong());
 		}
 		catch (RuntimeException e) {
 			comHome.rollbackTransaction();
@@ -287,4 +278,630 @@ class StreamsTest {
 		}
 		
 	}
+	
+	
+	/**
+	 * 8. Devuelve un listado con todos los pedidos que se han realizado.
+	 *  Los pedidos deben estar ordenados por la fecha de realización
+	 * , mostrando en primer lugar los pedidos más recientes
+	 */
+	@Test
+	void test8() {
+	
+		PedidoHome pedHome = new PedidoHome();	
+		try {
+			pedHome.beginTransaction();
+		
+			List<Pedido> list = pedHome.findAll();
+						
+			//TODO STREAMS
+			
+			
+			pedHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			pedHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 9. Devuelve todos los datos de los dos pedidos de mayor valor.
+	 */
+	@Test
+	void test9() {
+	
+		PedidoHome pedHome = new PedidoHome();	
+		try {
+			pedHome.beginTransaction();
+		
+			List<Pedido> list = pedHome.findAll();
+						
+			//TODO STREAMS
+			
+			pedHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			pedHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 10. Devuelve un listado con los identificadores de los clientes que han realizado algún pedido. 
+	 * Tenga en cuenta que no debe mostrar identificadores que estén repetidos.
+	 */
+	@Test
+	void test10() {
+		
+		PedidoHome pedHome = new PedidoHome();	
+		try {
+			pedHome.beginTransaction();
+		
+			List<Pedido> list = pedHome.findAll();
+						
+			
+			//TODO STREAMS
+			
+			pedHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			pedHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	/**
+	 * 11. Devuelve un listado con el nombre y los apellidos de los comerciales que tienen una comisión entre 0.05 y 0.11.
+	 * 
+	 */
+	@Test
+	void test11() {
+		
+		ComercialHome comHome = new ComercialHome();	
+		//PedidoHome pedHome = new PedidoHome();	
+		try {
+			//pedHome.beginTransaction();
+			comHome.beginTransaction();
+			
+			List<Comercial> list = comHome.findAll();		
+			
+			//TODO STREAMS
+			
+			comHome.commitTransaction();
+			
+		}
+		catch (RuntimeException e) {
+			comHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	
+	/**
+	 * 12. Devuelve el valor de la comisión de menor valor que existe para los comerciales.
+	 * 
+	 */
+	@Test
+	void test12() {
+		
+		ComercialHome comHome = new ComercialHome();	
+		//PedidoHome pedHome = new PedidoHome();	
+		try {
+			//pedHome.beginTransaction();
+			comHome.beginTransaction();
+			
+			List<Comercial> list = comHome.findAll();		
+			
+			//TODO STREAMS
+			
+			
+			comHome.commitTransaction();
+			
+		}
+		catch (RuntimeException e) {
+			comHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 13. Devuelve un listado de los nombres de los clientes que 
+	 * empiezan por A y terminan por n y también los nombres que empiezan por P. 
+	 * El listado deberá estar ordenado alfabéticamente.
+	 * 
+	 */
+	@Test
+	void test13() {
+		
+		ComercialHome comHome = new ComercialHome();	
+		//PedidoHome pedHome = new PedidoHome();	
+		try {
+			//pedHome.beginTransaction();
+			comHome.beginTransaction();
+			
+			List<Comercial> list = comHome.findAll();		
+			
+			//TODO STREAMS
+			
+			
+			comHome.commitTransaction();
+			
+		}
+		catch (RuntimeException e) {
+			comHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 14. Devuelve un listado de los nombres de los clientes 
+	 * que empiezan por A y terminan por n y también los nombres que empiezan por P. 
+	 * El listado deberá estar ordenado alfabéticamente.
+	 */
+	@Test
+	void test14() {
+		
+		ClienteHome cliHome = new ClienteHome();
+		
+		try {
+			cliHome.beginTransaction();
+	
+			List<Cliente> list = cliHome.findAll();
+			
+			//TODO STREAMS
+			
+			
+			cliHome.commitTransaction();
+			
+		}
+		catch (RuntimeException e) {
+			cliHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 15. Devuelve un listado de los clientes cuyo nombre no empieza por A. 
+	 * El listado deberá estar ordenado alfabéticamente por nombre y apellidos.
+	 */
+	@Test
+	void test15() {
+		
+		ClienteHome cliHome = new ClienteHome();
+		
+		try {
+			cliHome.beginTransaction();
+	
+			List<Cliente> list = cliHome.findAll();
+			
+			//TODO STREAMS
+			
+			
+			cliHome.commitTransaction();
+			
+		}
+		catch (RuntimeException e) {
+			cliHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	
+	/**
+	 * 16. Devuelve un listado con el identificador, nombre y los apellidos de todos 
+	 * los clientes que han realizado algún pedido. 
+	 * El listado debe estar ordenado alfabéticamente por apellidos y nombre y se deben eliminar los elementos repetidos.
+	 */
+	@Test
+	void test16() {
+		
+		PedidoHome pedHome = new PedidoHome();	
+		try {
+			pedHome.beginTransaction();
+		
+			List<Pedido> list = pedHome.findAll();
+						
+			
+			//TODO STREAMS
+			
+			pedHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			pedHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	/**
+	 * 17. Devuelve un listado que muestre todos los pedidos que ha realizado cada cliente. 
+	 * El resultado debe mostrar todos los datos del cliente primero junto con un sublistado de sus pedidos. 
+	 * El listado debe mostrar los datos de los clientes ordenados alfabéticamente por nombre y apellidos.
+	 * 
+Cliente [id=1, nombre=Aar�n, apellido1=Rivero, apellido2=G�mez, ciudad=Almer�a, categor�a=100]
+	Pedido [id=2, cliente=Cliente [id=1, nombre=Aar�n, apellido1=Rivero, apellido2=G�mez, ciudad=Almer�a, categor�a=100], comercial=Comercial [id=5, nombre=Antonio, apellido1=Carretero, apellido2=Ortega, comisi�n=0.12], total=270.65, fecha=2016-09-10]
+	Pedido [id=16, cliente=Cliente [id=1, nombre=Aar�n, apellido1=Rivero, apellido2=G�mez, ciudad=Almer�a, categor�a=100], comercial=Comercial [id=5, nombre=Antonio, apellido1=Carretero, apellido2=Ortega, comisi�n=0.12], total=2389.23, fecha=2019-03-11]
+	Pedido [id=15, cliente=Cliente [id=1, nombre=Aar�n, apellido1=Rivero, apellido2=G�mez, ciudad=Almer�a, categor�a=100], comercial=Comercial [id=5, nombre=Antonio, apellido1=Carretero, apellido2=Ortega, comisi�n=0.12], total=370.85, fecha=2019-03-11]
+Cliente [id=2, nombre=Adela, apellido1=Salas, apellido2=D�az, ciudad=Granada, categor�a=200]
+	Pedido [id=12, cliente=Cliente [id=2, nombre=Adela, apellido1=Salas, apellido2=D�az, ciudad=Granada, categor�a=200], comercial=Comercial [id=1, nombre=Daniel, apellido1=S�ez, apellido2=Vega, comisi�n=0.15], total=3045.6, fecha=2017-04-25]
+	Pedido [id=7, cliente=Cliente [id=2, nombre=Adela, apellido1=Salas, apellido2=D�az, ciudad=Granada, categor�a=200], comercial=Comercial [id=1, nombre=Daniel, apellido1=S�ez, apellido2=Vega, comisi�n=0.15], total=5760.0, fecha=2015-09-10]
+	Pedido [id=3, cliente=Cliente [id=2, nombre=Adela, apellido1=Salas, apellido2=D�az, ciudad=Granada, categor�a=200], comercial=Comercial [id=1, nombre=Daniel, apellido1=S�ez, apellido2=Vega, comisi�n=0.15], total=65.26, fecha=2017-10-05]
+	...
+	 */
+	@Test
+	void test17() {
+		
+		ClienteHome cliHome = new ClienteHome();
+		
+		try {
+			cliHome.beginTransaction();
+	
+			List<Cliente> list = cliHome.findAll();
+			
+			//TODO STREAMS
+			
+			
+			cliHome.commitTransaction();
+			
+		}
+		catch (RuntimeException e) {
+			cliHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 18. Devuelve un listado que muestre todos los pedidos en los que ha participado un comercial. 
+	 * El resultado debe mostrar todos los datos de los comerciales y el sublistado de pedidos. 
+	 * El listado debe mostrar los datos de los comerciales ordenados alfabéticamente por apellidos.
+	 */
+	@Test
+	void test18() {
+		
+		ComercialHome comHome = new ComercialHome();	
+		try {
+		
+			comHome.beginTransaction();
+			
+			List<Comercial> list = comHome.findAll();		
+		
+			
+			comHome.commitTransaction();
+			
+		}
+		catch (RuntimeException e) {
+			comHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 19. Devuelve el nombre y los apellidos de todos los comerciales que ha participado 
+	 * en algún pedido realizado por María Santana Moreno.
+	 */
+	@Test
+	void test19() {
+		
+		PedidoHome pedHome = new PedidoHome();	
+		try {
+			pedHome.beginTransaction();
+		
+			List<Pedido> list = pedHome.findAll();
+						
+			
+			//TODO STREAMS
+			
+			
+			pedHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			pedHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+
+	/**
+	 * 20. Devuelve un listado que solamente muestre los comerciales que no han realizado ningún pedido.
+	 */
+	@Test
+	void test20() {
+		
+		ComercialHome comHome = new ComercialHome();	
+		try {
+		
+			comHome.beginTransaction();
+			
+			List<Comercial> list = comHome.findAll();		
+		
+			//TODO STREAMS
+			
+			
+			comHome.commitTransaction();
+			
+		}
+		catch (RuntimeException e) {
+			comHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 21. Calcula el número total de comerciales distintos que aparecen en la tabla pedido
+	 */
+	@Test
+	void test21() {
+		
+		PedidoHome pedHome = new PedidoHome();	
+		try {
+			pedHome.beginTransaction();
+		
+			List<Pedido> list = pedHome.findAll();
+						
+			//TODO STREAMS
+			
+			
+			pedHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			pedHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	/**
+	 * 22. Calcula el máximo y el mínimo de total de pedido en un solo stream, transforma el pedido a un array de 2 double total, utiliza reduce junto con el array de double para calcular ambos valores.
+	 */
+	@Test
+	void test22() {
+		
+		PedidoHome pedHome = new PedidoHome();	
+		try {
+			pedHome.beginTransaction();
+		
+			List<Pedido> list = pedHome.findAll();
+						
+			//TODO STREAMS
+			
+
+			
+			pedHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			pedHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	
+	/**
+	 * 23. Calcula cuál es el valor máximo de categoría para cada una de las ciudades que aparece en cliente
+	 */
+	@Test
+	void test23() {
+		
+		ClienteHome cliHome = new ClienteHome();
+		
+		try {
+			cliHome.beginTransaction();
+	
+			List<Cliente> list = cliHome.findAll();
+			
+			//TODO STREAMS
+			
+			
+			cliHome.commitTransaction();
+			
+		}
+		catch (RuntimeException e) {
+			cliHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	
+	/**
+	 * 24. Calcula cuál es el máximo valor de los pedidos realizados 
+	 * durante el mismo día para cada uno de los clientes. Es decir, el mismo cliente puede haber 
+	 * realizado varios pedidos de diferentes cantidades el mismo día. Se pide que se calcule cuál es 
+	 * el pedido de máximo valor para cada uno de los días en los que un cliente ha realizado un pedido. 
+	 * Muestra el identificador del cliente, nombre, apellidos, la fecha y el valor de la cantidad.
+	 * Pista: utiliza collect, groupingBy, maxBy y comparingDouble métodos estáticos de la clase Collectors
+	 */
+	@Test
+	void test24() {
+		
+		PedidoHome pedHome = new PedidoHome();	
+		try {
+			pedHome.beginTransaction();
+		
+			List<Pedido> list = pedHome.findAll();
+						
+			//TODO STREAMS
+			
+			pedHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			pedHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	/**
+	 *  25. Calcula cuál es el máximo valor de los pedidos realizados durante el mismo día para cada uno de los clientes, 
+	 *  teniendo en cuenta que sólo queremos mostrar aquellos pedidos que superen la cantidad de 2000 €.
+	 *  Pista: utiliza collect, groupingBy, filtering, maxBy y comparingDouble métodos estáticos de la clase Collectors
+	 */
+	@Test
+	void test25() {
+		
+		PedidoHome pedHome = new PedidoHome();	
+		try {
+			pedHome.beginTransaction();
+		
+			List<Pedido> list = pedHome.findAll();
+					
+			//TODO STREAMS
+			
+			
+			pedHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			pedHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	/**
+	 *  26. Devuelve un listado con el identificador de cliente, nombre y apellidos 
+	 *  y el número total de pedidos que ha realizado cada uno de clientes durante el año 2017.
+	 * @throws ParseException 
+	 */
+	@Test
+	void test26() throws ParseException {
+		
+		ClienteHome cliHome = new ClienteHome();
+		
+		try {
+			cliHome.beginTransaction();
+	
+			List<Cliente> list = cliHome.findAll();
+			//PISTA: Generación por sdf de fechas
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			Date ultimoDia2016 = sdf.parse("2016-12-31");
+			Date primerDia2018 = sdf.parse("2018-01-01");
+			
+			//TODO STREAMS
+			
+			
+			cliHome.commitTransaction();
+			
+		}
+		catch (RuntimeException e) {
+			cliHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	
+	/**
+	 * 27. Devuelve cuál ha sido el pedido de máximo valor que se ha realizado cada año. El listado debe mostrarse ordenado por año.
+	 */
+	@Test
+	void test27() {
+		
+		PedidoHome pedHome = new PedidoHome();	
+		try {
+			pedHome.beginTransaction();
+		
+			List<Pedido> list = pedHome.findAll();
+					
+			Calendar calendar = Calendar.getInstance();
+			
+			
+			//TODO STREAMS
+					
+					
+			pedHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			pedHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	
+	/**
+	 *  28. Devuelve el número total de pedidos que se han realizado cada año.
+	 */
+	@Test
+	void test28() {
+		
+		PedidoHome pedHome = new PedidoHome();	
+		try {
+			pedHome.beginTransaction();
+		
+			List<Pedido> list = pedHome.findAll();
+					
+			Calendar calendar = Calendar.getInstance();
+			
+			//TODO STREAMS
+					
+							
+					
+			pedHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			pedHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	/**
+	 *  29. Devuelve los datos del cliente que realizó el pedido
+	 *  
+	 *   más caro en el año 2019.
+	 * @throws ParseException 
+	 */
+	@Test
+	void test29() throws ParseException {
+		
+		PedidoHome pedHome = new PedidoHome();	
+		try {
+			pedHome.beginTransaction();
+		
+			List<Pedido> list = pedHome.findAll();
+					
+			//PISTA: Generación por sdf de fechas
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			Date ultimoDia2018 = sdf.parse("2018-12-31");
+			Date primerDia2020 = sdf.parse("2020-01-01");
+			
+			//TODO STREAMS
+			
+				
+			pedHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			pedHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	
+	/**
+	 *  30. Calcula la estadísticas de total de todos los pedidos.
+	 *  Pista: utiliza collect con summarizingDouble
+	 */
+	@Test
+	void test30() throws ParseException {
+		
+		PedidoHome pedHome = new PedidoHome();	
+		try {
+			pedHome.beginTransaction();
+		
+			List<Pedido> list = pedHome.findAll();
+			
+			//TODO STREAMS
+				
+			pedHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			pedHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
 }
